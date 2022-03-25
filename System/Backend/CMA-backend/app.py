1a49acc8db91e14930379531f76fcd8c27c33714
Mohamed Ashraf <mohammedashraf@aucegypt.edu>
2022-03-25 23:28:17 +0200

    [+] APIs Done: -- CreateAdmin -- LoginAdmin -- CreateProctor -- LoginProctor -- CreateExamInstance -- proctor_Admin_assignment

--- System/Backend/CMA-backend/app.py
+++ System/Backend/CMA-backend/app.py
@@ -1,4 +1,5 @@
 from re import S
+import re
 from flask import Flask, jsonify,redirect,url_for,render_template,request
 import sqlalchemy, json,secrets
 from flask_sqlalchemy import SQLAlchemy
@@ -23,18 +24,21 @@ cheatingincidents = Base.classes.cheatingincidents
 frames = Base.classes.frames
 proctor = Base.classes.proctor
 examincidents = Base.classes.examIncidents
+proctorExamAssignment = Base.classes.proctor_adminstrator_assign
 ## important variables
 #this variable will be used to store passwords in the DB
 salt = None
 
+##############################################################
 
-    
+"""Admin-level APIs"""    
+
+#########################################################
 
 @app.route('/registerAdmin',methods=['POST'])
 def registerAdmin():
-    
+    """ @API Description: This API is used to reguister an admininstrator to the system """
     if request.method == "POST":
-        """ @API Description: This API is used to reguister an admininstrator to the system """
         #getting the request parameters
         NationalID = request.form['national_id']
         password = request.form['password']
@@ -56,13 +60,110 @@ def registerAdmin():
             return json.dumps({'status': 'fail', 'message': "User Already Exists"})
 
     return json.dumps({'status': 'Success', 'message': "Administrator Added Successfully!"})
+##############################################################
+@app.route('/loginAdmin',methods=['POST'])
+def loginAdmin():
+    """ @API Description: This API is used to login an admininstrator to the system """  
+    if request.method == "POST":
+        #getting the request parameters
+        NationalID = request.form['national_id']
+        password = request.form['password']
+        if "national_id" not in request.form or "password" not in request.form:
+                return json.dumps({'status': 'fail', 'message': 'Missing Parameter'})
+        try:
+            salt = password + app.config['SECRET_KEY']
+            db_pass = hashlib.md5(salt.encode()).hexdigest()
+            user =db.session.query(Admin).filter_by(NationalID=NationalID).first()
+            if user is None:
+                raise NotFound
+            if db_pass != user.passwd:
+                raise NotFound
+        except NotFound:
+            return json.dumps({'status': 'fail', 'message': "User Not Found"})
+        except sqlalchemy.exc.IntegrityError as e:
+            return json.dumps({'status': 'fail', 'message': "User Already Exists"})
+
+    return json.dumps({'status': 'Success', 'message': "Admin logged in successfully!"})
+
+#########################################################
+@app.route('/createExamInstance', methods=['POST'])
+def createExamInstance():
+    if request.method == "POST":
+        #getting the request parameters
+        ExamRoomID = request.form['examroom_id']
+        ExamID = request.form['exam_id']
+        SchoolName = request.form['school_name']
+        adminNatID = request.form['admin_national_id']
+        if "examroom_id" not in request.form or "exam_id" not in request.form or "school_name" not in request.form\
+            or "admin_national_id" not in request.form:
+                return json.dumps({'status': 'fail', 'message': 'Missing Parameter'})
+        try:
+            
+            # admin not found
+            user =db.session.query(Admin).filter_by(NationalID=adminNatID).first()
+            if user is None:
+                raise NotFound
+            
+            # else admin is found
+            newRoom = ExamRoom(ExamroomID = ExamRoomID, ExamID= ExamID, SchoolName=SchoolName,\
+                admin_national_id = adminNatID)
+            db.session.add(newRoom)
+            db.session.commit()
+        except NotFound:
+            return json.dumps({'status': 'fail', 'message': "Admin Not Found"})
+        except sqlalchemy.exc.IntegrityError as e:
+            return json.dumps({'status': 'fail', 'message': "exam instance Already Exists"})
+
+    return json.dumps({'status': 'Success', 'message': "exam instance created Successfully!"})
+    
+##############################################################
+@app.route('/proctorExamAssignment', methods=['POST'])
+def assignproctor_to_exam():
+    if request.method == "POST":
+        #getting the request parameters
+        adminNatID = request.form['admin_national_id']
+        proctorNatID = request.form['proctor_national_id']
+        AssignedExamRoomID = request.form['exam_room_id']
+        if "admin_national_id" not in request.form or "proctor_national_id" not in request.form or "exam_room_id" not in request.form:
+                return json.dumps({'status': 'fail', 'message': 'Missing Parameter'})
+        try:
+            
+            # admin not found
+            a =db.session.query(Admin).filter_by(NationalID=adminNatID).first()
+            if a is None:
+                raise NotFound
+            
+             # admin not found
+            p =db.session.query(proctor).filter_by(NationalID=proctorNatID).first()
+            if p is None:
+                raise NotFound
+            
+             # admin not found
+            eid =db.session.query(ExamRoom).filter_by(ExamroomID=AssignedExamRoomID).first()
+            if eid is None:
+                raise NotFound
+            
+            # else admin is found
+            newAssignment = proctorExamAssignment(admin_national_id = adminNatID, proctor_national_id= proctorNatID, \
+                AssignedExamRoomID = AssignedExamRoomID)
+            db.session.add(newAssignment)
+            db.session.commit()
+        except NotFound:
+            return json.dumps({'status': 'fail', 'message': "Admin or Proctor or Exam Room ID Not Found"})
+        except sqlalchemy.exc.IntegrityError as e:
+            return json.dumps({'status': 'fail', 'message': "Assignment Already Exists"})
+
+    return json.dumps({'status': 'Success', 'message': "Assignment Completed Successfully!"})
+    
+###############################################################
+
+"""user-level APIs"""
 
 ################################################################
 @app.route('/registerProctor',methods=['POST'])
 def registerProctor():
-    
+    """ @API Description: This API is used to register an invigilator to the system """
     if request.method == "POST":
-        """ @API Description: This API is used to register an invigilator to the system """
         #getting the request parameters
         NationalID = request.form['national_id']
         password = request.form['password']
@@ -85,30 +186,7 @@ def registerProctor():
             return json.dumps({'status': 'fail', 'message': "User Already Exists"})
 
     return json.dumps({'status': 'Success', 'message': "Proctor Added Successfully!"})
-###########################################################################
-@app.route('/loginAdmin',methods=['POST'])
-def loginAdmin():
-    if request.method == "POST":
-        """ @API Description: This API is used to login an admininstrator to the system """
-        #getting the request parameters
-        NationalID = request.form['national_id']
-        password = request.form['password']
-        if "national_id" not in request.form or "password" not in request.form:
-                return json.dumps({'status': 'fail', 'message': 'Missing Parameter'})
-        try:
-            salt = password + app.config['SECRET_KEY']
-            db_pass = hashlib.md5(salt.encode()).hexdigest()
-            user =db.session.query(Admin).filter_by(NationalID=NationalID).first()
-            if user is None:
-                raise NotFound
-            if db_pass != user.passwd:
-                raise NotFound
-        except NotFound:
-            return json.dumps({'status': 'fail', 'message': "User Not Found"})
-        except sqlalchemy.exc.IntegrityError as e:
-            return json.dumps({'status': 'fail', 'message': "User Already Exists"})
 
-    return json.dumps({'status': 'Success', 'message': "Admin logged in successfully!"})
 ######################################################################
 @app.route('/loginProctor',methods=['POST'])
 def loginProctor():
