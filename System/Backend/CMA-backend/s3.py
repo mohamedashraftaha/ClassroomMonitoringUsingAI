ce1a186e3d44ad2b5623ab37214a6b445a5388db
Mohamed Ashraf Taha <75078872+mohamedashraftaha@users.noreply.github.com>
2022-04-02 01:58:57 +0200

    Delete System/Backend/CMA-backend directory

--- System/Backend/CMA-backend/s3.py
+++
@@ -1,2 +0,0 @@
-access_key='AKIAWKAZELKAIHEHAREM'
-secret_access_key='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'
--- System/Backend/CMA-backend/userDefinedExceptions.py
+++
@@ -1,9 +0,0 @@
-
-# define Python user-defined exceptions
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-## user defined exceptions
-class NotFound(Error):
-    pass
