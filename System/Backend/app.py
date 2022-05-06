17441edf4ad3b2776d43ae930d7604c2b6b01c95
mohamedashraftaha <mohammedashraf@aucegypt.edu>
2022-05-06 17:21:56 +0200

    [+] fixed get frames links API

--- System/Backend/app.py
+++ System/Backend/app.py
@@ -14,14 +14,14 @@ salt = None
 
 '''Creating views for the database tables in the admin panel
 i.e:modelling how the tables' attributes will be displayed in the admin panel'''
-# a.add_view(MyModelView(db.admin, db.classroom_monitoring_db.session,endpoint='adminstrator',form_columns=['national_id', 'passwd', 'first_name','last_name','job_role'] ))    
-# a.add_view(MyModelView(db.proctor, db.classroom_monitoring_db.session,form_columns=['national_id', 'passwd', 'first_name','last_name','school_name']  ))
-# a.add_view(MyModelView(db.exam_instance, db.classroom_monitoring_db.session,form_columns=['exam_instance_id', 'exam_reference_code', 'school_name','admin_national_id','camera_static_ip','fps']  ))
-# a.add_view(MyModelView(db.admin_assign_proctor, db.classroom_monitoring_db.session,form_columns=['admin_national_id', 'proctor_national_id', 'exam_instance_id']  ))
-# a.add_view(MyModelView(db.proctor_monitor_exam, db.classroom_monitoring_db.session,form_columns=['proctor_national_id', 'exam_instance_id', 'model_sensitivity']  ))
-# a.add_view(MyModelView(db.exam_instance_cases, db.classroom_monitoring_db.session,form_columns=['case_id', 'exam_instance_id', 'stat','confidence','student_number']  ))
-# a.add_view(MyModelView(db.frames, db.classroom_monitoring_db.session,form_columns=['image_link', 'case_id','student_number','exam_instance_id']  ))
-# a.add_view(MyModelView(db.students_positions, db.classroom_monitoring_db.session,form_columns=['student_number', 'exam_instance_id', 'x','y','w','h']  ))
+a.add_view(MyModelView(db.admin, db.classroom_monitoring_db.session,endpoint='adminstrator',form_columns=['national_id', 'passwd', 'first_name','last_name','job_role'] ))    
+a.add_view(MyModelView(db.proctor, db.classroom_monitoring_db.session,form_columns=['national_id', 'passwd', 'first_name','last_name','school_name']  ))
+a.add_view(MyModelView(db.exam_instance, db.classroom_monitoring_db.session,form_columns=['exam_instance_id', 'exam_reference_code', 'school_name','admin_national_id','camera_static_ip','fps']  ))
+a.add_view(MyModelView(db.admin_assign_proctor, db.classroom_monitoring_db.session,form_columns=['admin_national_id', 'proctor_national_id', 'exam_instance_id']  ))
+a.add_view(MyModelView(db.proctor_monitor_exam, db.classroom_monitoring_db.session,form_columns=['proctor_national_id', 'exam_instance_id', 'model_sensitivity']  ))
+a.add_view(MyModelView(db.exam_instance_cases, db.classroom_monitoring_db.session,form_columns=['case_id', 'exam_instance_id', 'stat','confidence','student_number']  ))
+a.add_view(MyModelView(db.frames, db.classroom_monitoring_db.session,form_columns=['image_link', 'case_id','student_number','exam_instance_id']  ))
+a.add_view(MyModelView(db.students_positions, db.classroom_monitoring_db.session,form_columns=['student_number', 'exam_instance_id', 'x','y','w','h']  ))
 
 if __name__ == '__main__':
     user_level_api = userController.UserLevelAPIs()
